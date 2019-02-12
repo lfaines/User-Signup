2f695994389c96f21b7d6806a1f443b2cd8795d0
Blessed <codingdreams4ever@gmail.com>
2019-02-11 19:47:48 -0600

    completed redirect

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, redirect
 import cgi
 import os
@@ -7,126 +6,92 @@ import jinja2
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-signup = """
-<!doctype html>
-<html>
-    <head>
-        <title>User Sign-Up</title>
-    </head>
-    <body>
-        <h1> Sign-Up Form</h1><br>
-        <form action = 'validate-submission' method = 'post'>    
-           <label> Username:
-           {% if username is None %}
-           
-                <input type = "text" name = "username" value = "{username}"
-                {{username_error}}
-                {% endif %}
-               <!-- <h4 name = "username_error">{username_error}</h4>--> </>
-                <br>
-                <br>
-            <label>Password:
-                <input type = "password" name = "password"
-                <h4 name = "password_error">{password_error}</h4></>
-                <br>
-                <br>
-            <label>Verify Password:
-                <input type = "password" name = "verify_password"
-                <h4 name = "verify_password_error">{verify_password_error}</h4></>
-                <br>
-                <br>
-            <label>Email Address (optional):
-                <input type = "text" name = "email_address" value = "{email_address}"
-                <h4 name = "email_address_error">{email_address_error}</h4></>
-                <br>
-                <br>
-            <input type = "submit" />
-        </form>
-    </body>
-</html>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
 
 @app.route('/validate-submission')
 def display_signup_form():
-    return signup.format(username_error = '', password_error = '', email_address_error = '', username = '', password = '', verify_password = '', verify_password_error = '', email_address = '') 
+    template = jinja_env.get_template('index.html')
+    return template.render()
 
-@app.route('/validate-submission', methods = ['POST'])
+@app.route('/validate-submission', methods = ['POST', 'GET'])
 def validate_signup_form():
     username = request.form['username']
     password = request.form['password']
     verify_password = request.form['verify_password']
     email_address = request.form['email_address']
 
-    username_error = "You cannot leave space blank."
+    username_error = ''
     password_error = ''
     verify_password_error = ''
     email_address_error = ''
-    blank = ""
+    empty = ""
 
-    if password == blank or verify_password == blank or username == blank:
-      password_error ="You cannot leave space blank."
+    if password == empty or verify_password == empty:
+      password_error ="You cannot leave space empty."
       password = ""
-      verify_password_error ="You cannot leave space blank."
+      verify_password_error ="You cannot leave space empty."
       verify_password = ""
-      #username_error = "You cannot leave space blank."
-      username = ""
-    elif len(password) and len(verify_password) and len(username) < 3 or len(password) and len(verify_password) and len(username) > 20: 
+    elif len(password) and len(verify_password) < 3 or len(password) and len(verify_password) > 20: 
       password_error = "Passwords must be between 3-20 characters in length."
       password = ""
       verify_password_error = "Passwords must be between 3-20 characters in length."
       verify_password = ""
-      username_error = "Username must be between 3-20 characters in length."
-      username = ""
-    elif " " in password and verify_password and username:
+    elif " " in password and verify_password:
       password_error = "Passwords cannot have spaces."
       password = ""
       verify_password_error = "Passwords cannot have spaces."
       verify_password = ""
-      username_error = "Username cannot have spaces."
-      username = ""
     elif password != verify_password:
       password_error = "Passwords don't match."
       password = ""
       verify_password_error = "Passwords don't match."
       verify_password = ""
-    elif password == verify_password:
+    if password == verify_password:
       password = ""
       verify_password = ""
-  
- 
-    elif len(email_address) == 1 or len(email_address) == 2 or len(email_address) > 20:
-     email_address_error = "Email must be between 3-20 characters."
-     email_address = ""
+    
+    if username == "":
+      username_error = "You cannot leave space empty."
+      username = ""
+    elif len(username) < 3 or len(username) > 20: 
+      username_error = "Username must be between 3-20 characters in length."
+      username = ""
+    elif " " in username:
+      username_error = "Username cannot have spaces."
+      username = ""
+
+    if len(email_address) == 1 or len(email_address) == 2 or len(email_address) ==3 or len(email_address) > 20:
+      email_address_error = "Email must be between 3-20 characters."
+      email_address = ""
+    if len(email_address) == 0:
+      email_address = ""
+    elif not ("@") in email_address:
+      email_address_error = "Please enter a valid Email."
+      email_address = ""
+    elif not (".") in email_address:
+      email_address_error = "Please enter a valid Email."
+      email_address = ""
     elif email_address.count("@") > 1 or email_address.count(".") > 1 or email_address.count(" ") >= 1:
-        email_address_error = "Please enter a valid Email."
-        email_address = ""
-    elif not ("@") in email_address and len(email_address) > 3 and len(email_address) < 20:
-        email_address_error = "Please enter a valid Email."
-        email_address = ""
-    elif not (".") in email_address and len(email_address) > 3 and len(email_address) < 20:
-        email_address_error = "Please enter a valid Email."
-        email_address = ""
-    #elif len(email_address) == 0:
-      #email_address = ""
-        #email_address_error = ""
+      email_address_error = "Please enter a valid Email."
+      email_address = ""
     #else:
-    elif not username_error:
-        un = "Welcome, " + username + "!"
-        return redirect('/validated?un={0}'.format(un))
-    elif not password_error:
-        un = "Welcome, " + username + "!"
-        return redirect('validated?un={0}'.format(un))
-    elif not verify_password_error:
-        un = "Welcome, " + username + "!"
-        return redirect('validated?un={0}'.format(un))      
-    elif not email_address_error:
-        un = "Welcome, " + username + "!"
-        return redirect('validated?un={0}'.format(un))
-    elif not verify_password_error and not email_address_error: 
+    if username_error == "" and password_error== "" and verify_password_error== "" and email_address_error == "":
       un = "Welcome, " + username + "!"
       return redirect('/validated?un={0}'.format(un))
- 
-    return signup.format(username_error = username_error, password_error = password_error,email_address_error = email_address_error, username = username, password = password, verify_password = verify_password, verify_password_error = verify_password_error, email_address = email_address) 
+    #else:
+    #  template = jinja_env.get_template('index.html')
+    #  return template.render(username_error = username_error, password_error = password_error, #email_address_error = email_address_error, username = username, password = password, #verify_password = verify_password, verify_password_error = verify_password_error, email_address = #email_address) 
+#
+
+      #if not verify_password_error and not email_address_error: 
+      #  un = "Welcome, " + username + "!"
+      #  return redirect('/validated?un={0}'.format(un))
+    
+    template = jinja_env.get_template('index.html')
+    return template.render(username_error = username_error, password_error = password_error, email_address_error = email_address_error, username = username, password = password, verify_password = verify_password, verify_password_error = verify_password_error, email_address = email_address) 
+
 
 @app.route('/validated')
 def display_validated_form():
